404b45b7f7340bc24dd977ab2da431b1903f60c7
Eric Had Arach <hadaracheric@gmail.com>
2023-04-18 19:44:26 -0300

    Exercise 3

---
+++ javascript-exercises/exercise-3/script.js
@@ -0,0 +1,24 @@
+function calcularDiasCrecimiento(velocidadCrecimiento, velocidadDecrecimiento, alturaDeseada) {
+  
+  if (!(Number.isInteger(velocidadCrecimiento) && Number.isInteger(velocidadDecrecimiento), Number.isInteger(alturaDeseada))) {
+    return 'Todos los datos ingresados deben ser números enteros';
+  }
+  if (velocidadCrecimiento < 1 || velocidadDecrecimiento < 1 || alturaDeseada < 1) {
+    return 'Todos los datos ingresados deben ser números enteros positivos';
+  }
+  if (velocidadCrecimiento <= velocidadDecrecimiento) {
+    return 'La velocidad de crecimiento no puede ser igual o menor a la velocidad de decrecimiento';
+  }
+
+  let alturaActual = 0;
+  let cantDias = 0;
+
+  while (alturaDeseada > alturaActual) {
+    cantDias++;
+    alturaActual += velocidadCrecimiento - velocidadDecrecimiento;
+  }
+
+  return cantDias;
+}
+
+console.log(calcularDiasCrecimiento(3, 1, 11));
