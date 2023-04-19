9e6371d09ed7dfefe0344619262d7d245a955bae
Eric Had Arach <hadaracheric@gmail.com>
2023-04-19 20:57:49 -0300

    Addition of comment and change of operators == to ===

--- javascript-exercises/exercise-1/script.js
+++ javascript-exercises/exercise-1/script.js
@@ -10,6 +10,7 @@ const names = [
 
 function golfScore(par, strokes) {
   
+  // Validations
   if (typeof par !== 'number' || typeof strokes !== 'number') {
     return 'One of the values entered is not a number';
   }
@@ -19,11 +20,11 @@ function golfScore(par, strokes) {
 
   return (strokes == 1) ? names[0] :
     (strokes <= par - 2) ? names[1] :
-    (strokes == par - 1) ? names[2] :
-    (strokes == par) ? names[3] :
-    (strokes == par + 1) ? names[4] :
-    (strokes == par + 2) ? names[5] :
+    (strokes === par - 1) ? names[2] :
+    (strokes === par) ? names[3] :
+    (strokes === par + 1) ? names[4] :
+    (strokes === par + 2) ? names[5] :
     names[6];
 }
 
-console.loggolfScore(5, 4);
+golfScore(5, 4);
