64f64e912311c6a56d664da7081fc3b5dd08b266
Eric Had Arach <hadaracheric@gmail.com>
2023-04-19 00:03:20 -0300

    added difficulty

--- javascript-exercises/exercise-12/script.js
+++ javascript-exercises/exercise-12/script.js
@@ -1,22 +1,39 @@
 function jugar() {
   
+  let limiteDeIntentos;
+  do {
+    limiteDeIntentos = parseInt(prompt('Ingrese la cantidad máxima de intentos que desea'));
+  }while(limiteDeIntentos === 'NaN');
+
   let numeroAleatorio = 1 + Math.floor((Math.random()*10));
   let numeroJugador;
+  let intentos = 0;
 
   do {
     numeroJugador = parseInt(prompt('Ingrese número'));
-    if (numeroJugador === 'NaN') {
+    if (isNaN(numeroJugador)) {
+      console.log('ingreso');
       alert('Debes introducir un número válido');
     }
-    if (numeroJugador < numeroAleatorio) {
+    else if (numeroJugador < numeroAleatorio) {
       alert('El número es mayor');
+      intentos++;
     }
     else if (numeroJugador > numeroAleatorio) {
       alert('El número es menor');
+      intentos++;
     }
-  }while(numeroAleatorio !== numeroJugador);
+  }while((numeroAleatorio !== numeroJugador) && (intentos < limiteDeIntentos));
 
-  alert(`Haz acertado, el valor es ${numeroAleatorio}`);
+  if (intentos === limiteDeIntentos) {
+    alert('Perdiste, ya usaste todos los intentos permitidos');
+  }
+  else {
+    alert(`Haz acertado, el valor es ${numeroAleatorio}`);
+  }
 }
+
+jugar();
+
 
 
