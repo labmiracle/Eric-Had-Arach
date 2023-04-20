04cd6483e622eaab480ae2dd03fdc6d117cfafeb
Eric Had Arach <hadaracheric@gmail.com>
2023-04-19 21:01:26 -0300

    addition of comments

--- javascript-exercises/exercise-2/script.js
+++ javascript-exercises/exercise-2/script.js
@@ -1,11 +1,15 @@
 function sum(arr, n) {
+  
+  // Validation
   if (n <= 0 || n > arr.length) {
     return 'The number of items entered is incorrect';
   }
   
+  // Base case
   if (n === 1) {
     return arr[0];
   }
+  
   return arr[n-1] + sum(arr, n-1);  
 }
 
