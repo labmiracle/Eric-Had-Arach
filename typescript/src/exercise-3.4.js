85d026e045f04ce73aad1181d42661728c9decd1
Eric Had Arach <hadaracheric@gmail.com>
2023-05-07 00:45:24 -0300

    Indentation correction

--- typescript/src/exercise-3.4.js
+++ typescript/src/exercise-3.4.js
@@ -1,7 +1,7 @@
 function greet(greeting) {
-    if (greeting === void 0) { greeting = "hola"; }
-    /* si indico que greeting es tipo string, al estar el parámetro por defecto "hola" da el siguiente error: "Type string trivially inferred from a string literal, remove type annotation.", por eso lo omito*/
-    return greeting.toUpperCase();
+	if (greeting === void 0) { greeting = "hola"; }
+	/* si indico que greeting es tipo string, al estar el parámetro por defecto "hola" da el siguiente error: "Type string trivially inferred from a string literal, remove type annotation.", por eso lo omito*/
+	return greeting.toUpperCase();
 }
 var defaultGreeting = greet();
 var portugueseGreeting = greet("Oi como vai!");
