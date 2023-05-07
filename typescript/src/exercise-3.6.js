2f2407693d4143ea45d8ba3eee84d6054244b1f4
Eric Had Arach <hadaracheric@gmail.com>
2023-05-07 01:10:12 -0300

    Exercise 3.6

--- typescript/src/exercise-3.6.js
+++ typescript/src/exercise-3.6.js
@@ -1,9 +1,9 @@
 var multiply;
 var capitalize;
 multiply = function (x, y) {
-    return x * y;
+	return x * y;
 };
 capitalize = function (value) {
-    return "".concat(value.charAt(0).toUpperCase()).concat(value.slice(1));
+	return "".concat(value.charAt(0).toUpperCase()).concat(value.slice(1));
 };
 console.log("[Ejercicio 3.6]", capitalize("habil ".concat(multiply(5, 10))));
