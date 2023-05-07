ed009bb0293ece21e88d307c6baa2b410be23a64
Eric Had Arach <hadaracheric@gmail.com>
2023-05-06 22:36:04 -0300

    Bonus item 3

--- typescript/src/exercise-2.3.js
+++ typescript/src/exercise-2.3.js
@@ -17,3 +17,23 @@ function flatten(array) {
 }
 var flattenedNumbers = flatten(numbers);
 console.log("[Ejercicio 4.3]", flattenedNumbers);
+// BONUS (item 3)
+var numbers2 = [1, 2, 3, [44, 55], 6, [77, 88], 9, 10];
+function flatten2(array) {
+	var flattened2 = [];
+	for (var _i = 0, array_2 = array; _i < array_2.length; _i++) {
+		var element = array_2[_i];
+		if (Array.isArray(element)) {
+			element; // any[] 
+			flattened2.push.apply(// any[] 
+				flattened2, element);
+		}
+		else {
+			element; // any 
+			flattened2.push(element);
+		}
+	}
+	return flattened2;
+}
+var flattenedNumbers2 = flatten2(numbers2);
+console.log("[Ejercicio 4.3 item 3]", flattenedNumbers2);
--- typescript/src/exercise-2.3.ts
+++ typescript/src/exercise-2.3.ts
@@ -19,3 +19,29 @@ function flatten(array: (number | number[])[]) {
 const flattenedNumbers = flatten(numbers);
 
 console.log("[Ejercicio 4.3]", flattenedNumbers);
+
+
+
+// BONUS (item 3)
+
+const numbers2: (number | number[])[] = [1, 2, 3, [44, 55], 6, [77, 88], 9, 10];
+
+function flatten2<T>(array: (T | T[])[]) {
+	const flattened2: T[] = [];
+    
+	for (const element of array) { 
+		if (Array.isArray(element)) { 
+			element; // any[] 
+			flattened2.push(...element); 
+		} else { 
+			element; // any 
+			flattened2.push(element); 
+		} 
+	}
+    
+	return flattened2;
+}
+
+const flattenedNumbers2 = flatten2(numbers2);
+
+console.log("[Ejercicio 4.3 item 3]", flattenedNumbers2);
