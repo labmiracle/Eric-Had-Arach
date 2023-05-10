d21310b24b4e2bdfb1f62452785cc3df7b26bcad
Eric Had Arach <hadaracheric@gmail.com>
2023-05-10 12:26:28 -0300

    Addition exercise 3

---
+++ typescript/src/Additional-exercises/additional-exercise-3-cartas.ts
@@ -0,0 +1,37 @@
+
+function createCards(): number[] {
+	const quantityOfCards = 10;
+	const cards: number[] = Array(10);
+	for (let i = 0; i < quantityOfCards; i++) {
+		let randomNumber: number = Math.floor(Math.random() * 10) + 1;
+		while (cards.includes(randomNumber)) {
+			randomNumber = Math.floor(Math.random() * 10) + 1;
+		}
+		cards.push(randomNumber);
+	}
+	return cards;
+}
+
+function extractCardPlayer1(cards: number[]): number {
+	return cards.pop();
+}
+
+function extractCardPlayer2(cards: number[]): number {
+	return cards.pop();
+}
+
+function playGame(): void {
+	const cards: number[] = createCards();
+	const cardPlayer1: number = extractCardPlayer1(cards);
+	const cardPlayer2: number = extractCardPlayer2(cards);
+	if (cardPlayer1 > cardPlayer2) {
+		console.log("Player 1 wins");
+	}
+	else {
+		console.log("Player 2 wins");
+	}
+}
+
+playGame();
+
+
