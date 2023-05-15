1e8c4cde701998c6847d609895c5cf28a85bcc4e
Eric Had Arach <hadaracheric@gmail.com>
2023-05-15 19:49:01 -0300

    Removal of .js files and addition of possible return undefined in aditional exercise 3

--- typescript/src/Additional-exercises/additional-exercise-3-cartas.ts
+++ typescript/src/Additional-exercises/additional-exercise-3-cartas.ts
@@ -12,19 +12,19 @@ function createCards(): number[] {
 	return cards;
 }
 
-function extractCardPlayer1(cards: number[]): number {
+function extractCardPlayer1(cards: number[]): number | undefined {
 	return cards.pop();
 }
 
-function extractCardPlayer2(cards: number[]): number {
+function extractCardPlayer2(cards: number[]): number | undefined {
 	return cards.pop();
 }
 
 function playGame(): void {
 	const cards: number[] = createCards();
-	const cardPlayer1: number = extractCardPlayer1(cards);
-	const cardPlayer2: number = extractCardPlayer2(cards);
-	if (cardPlayer1 > cardPlayer2) {
+	const cardPlayer1 = extractCardPlayer1(cards);
+	const cardPlayer2 = extractCardPlayer2(cards);
+	if (cardPlayer1 !== undefined && cardPlayer2 !== undefined && cardPlayer1 > cardPlayer2) {
 		console.log("Player 1 wins the game");
 	}
 	else {
