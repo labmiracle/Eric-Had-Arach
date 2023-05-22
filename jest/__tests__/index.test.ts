7fcde2b6f12089be64f34d233ba1d0e8487e93d9
Eric Had Arach <hadaracheric@gmail.com>
2023-05-22 11:44:49 -0300

    Exercise 1.1

--- jest/__tests__/index.test.ts
+++ jest/__tests__/index.test.ts
@@ -1,1 +1,19 @@
 import { fizzBuzz } from "../src/exercise-1.1";
+
+describe("Exercise 1.1", () => {
+    test("is divisible by 3", () => {
+        expect(fizzBuzz(3)).toBe("Fizz");
+        expect(fizzBuzz(6)).toBe("Fizz");
+    });
+    test("is divisible by 5", () => {
+        expect(fizzBuzz(5)).toBe("Buzz");
+        expect(fizzBuzz(10)).toBe("Buzz");
+    });
+    test("is divisible by 3 and 5", () => {
+        expect(fizzBuzz(15)).toBe("FizzBuzz");
+    });
+    test("no 0 was entered", () => {
+        expect(fizzBuzz(0)).toBe(0);
+    })
+});
+// falta el supuesto de que se pase un string
