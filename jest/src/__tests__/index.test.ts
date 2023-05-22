eb32a895b72965b8859685739cfc6a1af0724e42
Eric Had Arach <hadaracheric@gmail.com>
2023-05-22 13:09:44 -0300

    Exercise 1.4

--- jest/src/__tests__/index.test.ts
+++ jest/src/__tests__/index.test.ts
@@ -1,6 +1,7 @@
 import { fizzBuzz } from "../exercise-1.1";
 import { isLeapYear } from "../exercise-1.2";
 import { getADN } from "../exercise-1.3";
+import { sumNumbers } from "../exercise-1.4";
 
 describe("Exercise 1.1", () => {
     test("is divisible by 3", () => {
@@ -34,11 +35,25 @@ describe("Exercise 1.2", () => {
 });
 
 describe("Exercise 1.3", () => {
-    test("not empty string", () => {
+    test("empty string", () => {
         expect(getADN("")).toBe("");
     });
     test("test", () => {
         expect(getADN("CTAGGGTA")).toBe("CTAG");
+    });
+});
+
+describe("Exercise 1.4", () => {
+    test("empty array", () => {
+        expect(sumNumbers([])).toBe(0);
+    });
+    test("tests", () => {
+        expect(sumNumbers([1,2,3,4])).toBe(10);
+        expect(sumNumbers([1,-2,-3,5])).toBe(6);
+        expect(sumNumbers([1,"2",3,4])).toBe(10);
+        expect(sumNumbers([1,"-2",3,4])).toBe(8);
+        expect(sumNumbers([1,"asd",3,4])).toBe(8);
+        
     });
 });
 
---
+++ jest/src/exercise-1.4.ts
@@ -0,0 +1,22 @@
+export function sumNumbers(values: any[]): number {
+    if (values.length === 0) {
+        return 0;
+    }
+    let result = 0;
+    for (let i=0; i < values.length; i++) {
+        if (typeof values[i] !== "number") {
+            if (Number(values[i])) {
+                const value = Number(values[i]);
+                if (value > 0) {
+                    result += value;
+                    continue;
+                }
+
+            }
+        }
+        if (values[i] > 0) {
+            result += values[i];
+        }
+    }
+    return result;
+}
