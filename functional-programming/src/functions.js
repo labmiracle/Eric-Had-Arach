337926cf4c140dd54aad51fa1801908922874ba3
Eric Had Arach <hadaracheric@gmail.com>
2023-06-01 12:36:28 -0300

    item 3 reduce

--- functional-programming/src/functions.js
+++ functional-programming/src/functions.js
@@ -1,68 +1,56 @@
-const characters = [
+var characters = [
     {
         name: 'Luke Skywalker',
         height: '172',
         mass: '77',
         eye_color: 'blue',
-        gender: 'male',
+        gender: 'male'
     },
     {
         name: 'Darth Vader',
         height: '202',
         mass: '136',
         eye_color: 'yellow',
-        gender: 'male',
+        gender: 'male'
     },
     {
         name: 'Leia Organa',
         height: '150',
         mass: '49',
         eye_color: 'brown',
-        gender: 'female',
+        gender: 'female'
     },
     {
         name: 'Anakin Skywalker',
         height: '188',
         mass: '84',
         eye_color: 'blue',
-        gender: 'male',
+        gender: 'male'
     }
 ];
-
 //--------------MAP---------------
-
 // Obtener un arreglo de todos los nombres
-const names = characters.map(elem => elem.name);
-
+var names = characters.map(function (elem) { return elem.name; });
 // Obtener un arreglo de todas las alturas
-const heights = characters.map(elem => elem.height);
-
+var heights = characters.map(function (elem) { return elem.height; });
 // Obtener un arreglo de objetos con las propiedades de nombre y altura
-const namesAndHeights = characters.map(elem => {
+var namesAndHeights = characters.map(function (elem) {
     return {
-        name: elem.name, 
+        name: elem.name,
         height: elem.height
-    }
+    };
 });
-
-
 //--------------REDUCE---------------
-
 // Obtener la masa total de todos los personajes
-const totalMass = characters.reduce((accumulator, elem) => {
+var totalMass = characters.reduce(function (accumulator, elem) {
     return accumulator + Number(elem.mass);
 }, 0);
-
 // Obtener la altura total de todos los caracteres
-const totalHeights = characters.reduce((accumulator, elem) => {
+var totalHeights = characters.reduce(function (accumulator, elem) {
     return accumulator + Number(elem.height);
 }, 0);
-
 // Obtener el número total de caracteres en todos los nombres de los personajes
-const totalCharactersNames = characters.reduce((accumulator, elem) => {
+var totalCharactersNames = characters.reduce(function (accumulator, elem) {
     return accumulator + elem.name.length;
 }, 0);
-
 // Obtener el número total de caracteres por color de ojos (pista: un mapa de color de ojos para contar)
-
-
