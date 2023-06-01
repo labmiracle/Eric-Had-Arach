ab2238c08b619fbbd9f080d20070bc52251133f4
Eric Had Arach <hadaracheric@gmail.com>
2023-06-01 12:00:02 -0300

    Array with all names

---
+++ functional-programming/src/map.js
@@ -0,0 +1,33 @@
+var characters = [
+    {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        eye_color: 'blue',
+        gender: 'male'
+    },
+    {
+        name: 'Darth Vader',
+        height: '202',
+        mass: '136',
+        eye_color: 'yellow',
+        gender: 'male'
+    },
+    {
+        name: 'Leia Organa',
+        height: '150',
+        mass: '49',
+        eye_color: 'brown',
+        gender: 'female'
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: '188',
+        mass: '84',
+        eye_color: 'blue',
+        gender: 'male'
+    }
+];
+// Obtener un arreglo de todos los nombres
+var names = characters.map(function (elem) { return elem.name; });
+console.log(names);
