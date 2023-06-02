fece57a811e8dc594f12a07134f6497910fa9e0e
Eric Had Arach <hadaracheric@gmail.com>
2023-06-02 16:08:49 -0300

    Own reduce function

--- functional-programming/src/additional-exercises/own-reduce-function.ts
+++ functional-programming/src/additional-exercises/own-reduce-function.ts
@@ -1,11 +1,27 @@
 // Implemente su propia función reduce
 
-Array.prototype.myReduce = function(callback) {
-    const array = [];
-    for (let i = 0; i < this.length; i++) {
-        if (callback(this[i])) {
-            array.push(this[i]);
+Array.prototype.myReduce = function(callback, initialValue) {
+    let accumulator;
+    let thereIsInitialValue;
+    
+    if (initialValue === "undefined") {
+        thereIsInitialValue = false;
+        accumulator = this[0];
+    }
+    else {
+        thereIsInitialValue = true;
+        accumulator = initialValue;
+    }
+
+    if (thereIsInitialValue) {
+        for (let i = 0; i < this.length; i++) {
+            accumulator = callback(accumulator, this[i]);
         }
     }
-    return array;
+    else {
+        for (let i = 1; i < this.length; i++) {
+            accumulator = callback(accumulator, this[i]);
+        }
+    }
+    return accumulator;
 }
