6d8f6499b558cb88e7d23c264f9c32afc6e32771
Eric Had Arach <hadaracheric@gmail.com>
2023-06-02 21:41:11 -0300

    improved readability

--- functional-programming/src/exercise.ts
+++ functional-programming/src/exercise.ts
@@ -1,24 +1,38 @@
 const users = [
     { 
-        firstName: "Bradley", lastName: "Bouley", role: "Full Stack Resident" 
+        firstName: "Bradley", 
+        lastName: "Bouley", 
+        role: "Full Stack Resident" 
     },
     { 
-        firstName: "Chloe", lastName: "Alnaji", role: "Full Stack Resident"
+        firstName: "Chloe", 
+        lastName: "Alnaji", 
+        role: "Full Stack Resident"
     },
     { 
-        firstName: "Jonathan", lastName: "Baughn", role: "Enterprise Instructor"
+        firstName: "Jonathan", 
+        lastName: "Baughn", 
+        role: "Enterprise Instructor"
     },
     { 
-        firstName: "Michael", lastName: "Herman", role: "Lead Instructor"
+        firstName: "Michael", 
+        lastName: "Herman", 
+        role: "Lead Instructor"
     },
     { 
-        firstName: "Robert", lastName: "Hajek", role: "Full Stack Resident"
+        firstName: "Robert", 
+        lastName: "Hajek", 
+        role: "Full Stack Resident"
     },
     { 
-        firstName: "Wes", lastName: "Reid", role: "Instructor"
+        firstName: "Wes", 
+        lastName: "Reid", 
+        role: "Instructor"
     },
     { 
-        firstName: "Zach", lastName: "Klabunde", role: "Instructor"
+        firstName: "Zach", 
+        lastName: "Klabunde", 
+        role: "Instructor"
     },
 ];
 
