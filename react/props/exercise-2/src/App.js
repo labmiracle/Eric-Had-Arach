583f2355915c325cd7812ce4abaae7b0e9b1d2db
Eric Had Arach <hadaracheric@gmail.com>
2023-06-12 08:38:04 -0300

    Exercise 2

---
+++ react/props/exercise-2/src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+
+function Title({children}) {
+    return (
+        <h1>{children}</h1>
+    );
+}
+
+function Text({children}) {
+    return (
+        <p>{children}</p>
+    );
+}
+
+function List({children}) {
+    return (
+        <ul>{children}</ul>
+    );
+}
+
+function ListItem({children}) {
+    return (
+        <li>{children}</li>
+    );
+}
+
+function Link({link, newTab, children}) {
+    return (
+        <a href={link} target={newTab ? "_blank" : "_self"} rel="noreferrer">{children}</a>
+    );
+}
+
+function Loading({show, children}) {
+    return (
+        show ? <>{children}</> : <p>Loading</p>
+    );
+}
+
+function App() {
+  return (
+    <>
+        <Loading show={true}>
+            <Title>
+                Necesito partir en componentes todo esto
+            </Title>
+            <Text>
+                Para ello puedo usar React que me permitirá reutilizar todos esos
+    componentes. Para ello tengo que:
+            </Text>
+            <List>
+                <ListItem>Observar el HTML</ListItem>
+                <ListItem>Pensar en cómo puedo extraer cada parte en componentes</ListItem>
+                <ListItem>Usarlos, o mejor dicho, reusarlos en React</ListItem>
+            </List>
+            <Link
+                link="https://react.dev/"
+                newTab={true}>
+                    React Docs
+            </Link>
+        </Loading>     
+    </>
+  );
+}
+
+export default App;
