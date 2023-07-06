7b999053bc0cfa4b153da939274cea2afc4c6168
Eric Had Arach <hadaracheric@gmail.com>
2023-07-06 09:44:09 -0300

    Set exercises

--- data-structures/set/src/index.ts
+++ data-structures/set/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hola');
+//Exercise 1
+function areThereRepeats<T>(values: T[]): boolean {
+    return (values.length === new Set(values).size) ? false : true;
+}
+
+
+//Exercise 2
+function getUniqueItems(arr1: number[], arr2: number[]): number[] {
+    const nonRepeatedValues = new Set([...arr1, ...arr2]);
+    return Array.from(nonRepeatedValues);
+}
